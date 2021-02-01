e56c6bc0c7527365612d26911448eb0c2664ef35
tNahoko <nahoko.toyota@gmail.com>
2021-02-01 22:53:33 +0900

    add POST

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/scrum", async (_, res) => {
+app.get("/api/scrum", async (req, res) => {
   const data = await knex.select().table("scrum");
   res.status(200);
   res.send(data);
+});
+
+app.post("/api/scrum", async (req, res) => {
+  try {
+    const newMember = {
+    name : req.body.name,
+    role : req.body.role,
+    level : req.body.level
+  };
+    await knex('scrum').insert(newMember);
+    const added = await knex('scrum').select().where('name', req.body.name);
+    res.status(200).send(added);
+  } catch (error) {
+    res.sendStatus(400);
+  }
 });
 
 const port = process.env.PORT || 3000;
