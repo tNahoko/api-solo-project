678550e09d1dedb150eedda87f42e4fbe4f42b7a
tNahoko <nahoko.toyota@gmail.com>
2021-02-01 18:03:52 +0900

    add app.get in server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 9999;
+app.get("/api/scrum", async (_, res) => {
+  const data = await knex.select().table("scrum");
+  res.status(200);
+  res.send(data);
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server running at " + port);
 });
